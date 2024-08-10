c0b713fa7482ce422ac54116f04831f9349ad572
Steven Webb <u7544998@anu.edu.au>
2024-08-10 16:37:10 +1000

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './styles/style.scss';
+
 // Lazy load each section
 const Home = lazy(() => import('./pages/Home/Home'));
 
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-//import { SuperHeader, Navbar } from '../../components/NavBar/Navbar';
-//import Footer from '../../components/Footer/Footer';
+import { Navbar } from '../../components/NavBar/Navbar';
+import Footer from '../../components/Footer/Footer'; 
 
 //import EventFilter from '../../components/EventFilter/EventFilter';
 //import EventList from '../../components/EventList/EventList';
@@ -9,10 +9,10 @@ function Home() {
     return (
         <div id="app">
             
-            Home
+            <Navbar />
             { /*
             <SuperHeader />
-            <Navbar />
+            
             
             <div className="content">
                 <div className="container-fluid">
@@ -23,8 +23,10 @@ function Home() {
                 </div>
             </div>
 
-            <Footer />
+            
             */ }
+
+            <Footer />
         </div>
     );
 }
