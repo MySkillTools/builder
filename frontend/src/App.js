8c98a98528684d63d03e466ed0b5f24e59e94c73
Steven Webb <u7544998@anu.edu.au>
2024-08-10 16:04:36 +1000

    react router test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-//import logo from './logo.svg';
-//import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+
+// Lazy load each section
+const Home = lazy(() => import('./pages/Home/Home'));
 
 function App() {
     return (
-        <div className="App">
-            Hello World!
-        </div>
+        <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Suspense>
+        </Router>
     );
 }
 
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+//import { SuperHeader, Navbar } from '../../components/NavBar/Navbar';
+//import Footer from '../../components/Footer/Footer';
+
+//import EventFilter from '../../components/EventFilter/EventFilter';
+//import EventList from '../../components/EventList/EventList';
+
+function Home() {
+    return (
+        <div id="app">
+            
+            Home
+            { /*
+            <SuperHeader />
+            <Navbar />
+            
+            <div className="content">
+                <div className="container-fluid">
+                    <div className="row">
+                        <EventFilter />
+                        <EventList />
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+            */ }
+        </div>
+    );
+}
+
+
+export default Home;
